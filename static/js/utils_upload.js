8199a6e47f10bade944e7376798a889cfb4d6841
jerrylei98 <jerrylei98@gmail.com>
2016-09-08 20:46:09 -0400

    file limitation!

--- static/js/utils_upload.js
+++ static/js/utils_upload.js
@@ -1,3 +1,4 @@
+/*
 window.onload = function start(){
   setInterval(show_file, 50);
 }
@@ -9,7 +10,48 @@ function show_file(){
   file_name = file_name.split("\\");
   file_name = file_name[file_name.length - 1];
   document.getElementById("file_name").innerHTML=file_name;
-  if (uploaded_file.size > 200){
-      console.log("File size must under 2mb!");
-  }
 }
+*/
+var control = document.getElementById("file_upload");
+control.addEventListener("change", function(event) {
+  var i = 0,
+    files = control.files,
+    len = files.length;
+
+  var size_check = false;
+  var type_check = false;
+  for (; i < len; i++) {
+    if(files[i].type.indexOf("image/") != -1){ //if filetype is image
+      type_check = true;
+    }
+    else{
+      type_check = false;
+    }
+    if(files[i].size < 5242880){
+      size_check = true;
+    }
+    else{
+      size_check = false;
+    }
+    if(size_check == false){
+      var delete_upload = document.getElementById('check_upload_div');
+      if(delete_upload != null){
+        delete_upload.parentNode.removeChild(delete_upload);
+      }
+    }
+    if(type_check == false){
+      var delete_upload = document.getElementById('check_upload_div');
+      if(delete_upload != null){
+        delete_upload.parentNode.removeChild(delete_upload);
+      }
+    }
+    if(size_check == true && type_check == true){
+      var checked_upload = document.createElement("div");
+      checked_upload.setAttribute("id", "check_upload_div");
+      checked_upload.innerHTML = "<input type='submit' class='checked_upload' value='Upload' name='button' value='upload_button'>";
+      document.getElementById("upload_form").appendChild(checked_upload);
+    }
+    document.getElementById("file_name").innerHTML=files[i].name;
+  }
+
+}, false);
